edaae759172ff35d8757075a665184c68ba6e2d3
morcelicaio <morcelicaio@hotmail.com>
2024-07-05 01:38:17 -0300

    Atualiza a criação do model User

--- src/main/java/com/caiomorceli/todosimple/model/User.java
+++ src/main/java/com/caiomorceli/todosimple/model/User.java
@@ -6,6 +6,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "user")
@@ -15,10 +23,87 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    
+
     @Column(name = "username", length = 50, nullable = false, unique = true)
+    @NotNull  // Não aceita valor null no atributo.
+    @NotEmpty // Não aceita string vazia no atributo.
     private String username;
+
     @Column(name = "password", length = 50, nullable = false)
+    @JsonProperty(access = Access.WRITE_ONLY) // Atributo apenas de escrita. A api não retornará esse atributo no json.
+    @NotNull
+    @NotEmpty
+    @Size(min = 7, max = 50) // O password deve ter no mínimo e no máximo 50 caracteres.
     private String password;
 
+    public User(){
+
+    }
+
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == this){
+            return true;
+        }
+        if(obj == null){
+            return false;
+        }
+        if(!(obj instanceof User)){
+            return false;
+        } 
+        
+        User other = (User) obj;
+
+        if(this.id == null){
+            if(other.id != null){
+                return false;
+            }   else if(!this.id.equals(other.id)){
+                    return false;
+                }
+        }
+
+        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username) && Objects.equals(this.password, other.password);
+
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+
+        return result;
+    }
 }
