3e65e4515084174e96cb72fc5d0437d84bfbcb4c
morcelicaio <morcelicaio@hotmail.com>
2024-07-05 02:53:41 -0300

    Cria o repository User

---
+++ src/main/java/com/caiomorceli/todosimple/repositorie/UserRepository.java
@@ -0,0 +1,11 @@
+package com.caiomorceli.todosimple.repositorie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.caiomorceli.todosimple.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
