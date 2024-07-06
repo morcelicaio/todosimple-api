402e0ec8cdbe4d34f6872895906bfbd62ec28965
morcelicaio <morcelicaio@hotmail.com>
2024-07-06 17:47:57 -0300

    Refatora o model User para a criacao do Controller User

--- src/main/java/com/caiomorceli/todosimple/model/User.java
+++ src/main/java/com/caiomorceli/todosimple/model/User.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -21,6 +22,14 @@ import java.util.Objects;
 @Entity
 @Table(name = "user")
 public class User {
+
+    public interface CreateUser{ 
+
+    }
+
+    public interface UpdateUser{
+
+    }
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,15 +37,15 @@ public class User {
     private Long id;
 
     @Column(name = "username", length = 50, nullable = false, unique = true)
-    @NotNull  // Não aceita valor null no atributo.
-    @NotEmpty // Não aceita string vazia no atributo.
+    @NotNull(groups = CreateUser.class)  // Não aceita valor null no atributo.
+    @NotEmpty(groups = CreateUser.class) // Não aceita string vazia no atributo.
     private String username;
 
     @Column(name = "password", length = 50, nullable = false)
     @JsonProperty(access = Access.WRITE_ONLY) // Atributo apenas de escrita. A api não retornará esse atributo no json.
-    @NotNull
-    @NotEmpty
-    @Size(min = 7, max = 50) // O password deve ter no mínimo e no máximo 50 caracteres.
+    @NotNull(groups = { CreateUser.class, UpdateUser.class })
+    @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+    @Size(groups = { CreateUser.class, UpdateUser.class }, min = 7, max = 50) // O password deve ter no mínimo 7 e no máximo 50 caracteres.
     private String password;
 
     @OneToMany(mappedBy = "user")
@@ -78,7 +87,7 @@ public class User {
         this.password = password;
     }
 
-
+    @JsonIgnore
     public List<Task> getTasks() {
         return this.tasks;
     }
