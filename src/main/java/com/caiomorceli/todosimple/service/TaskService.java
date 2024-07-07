8a2470b6790fa3c66636fa553767aaacbcc7dfee
morcelicaio <morcelicaio@hotmail.com>
2024-07-07 12:12:58 -0300

    Adciona o método de retorno de todas as tasks de um usuário

--- src/main/java/com/caiomorceli/todosimple/service/TaskService.java
+++ src/main/java/com/caiomorceli/todosimple/service/TaskService.java
@@ -1,5 +1,6 @@
 package com.caiomorceli.todosimple.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,12 @@ public class TaskService {
 
         return task.orElseThrow(() -> new RuntimeException(
             "Tarefa não encontrada! \n Id: "+ id + ", Tipo: "+Task.class.getName()));
+    }
+
+    public List<Task> findAllTasksByUserId(Long userId){
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        
+        return tasks;
     }
 
     @Transactional
