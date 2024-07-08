64b0935162c3ecdf96ba71f3620a8dac2d683024
morcelicaio <morcelicaio@hotmail.com>
2024-07-08 00:20:07 -0300

    Cria a classe WebConfig para configuração básica do CORS

---
+++ src/main/java/com/caiomorceli/todosimple/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.caiomorceli.todosimple.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Arquivo de configuração CORS.
+
+@Configuration
+@EnableWebMvc   // habilita as configurações padrão do Spring MVC, e registra os componentes básicos necessários
+public class WebConfig implements WebMvcConfigurer {
+    
+    // Liberado para conexão de qualquer local (configuração simples). 
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }
+}
