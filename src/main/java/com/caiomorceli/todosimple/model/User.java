b53f1b8b1e254c15f03f8cc3f4e84718b4d451fd
morcelicaio <morcelicaio@hotmail.com>
2024-07-10 02:19:07 -0300

    Adiciona o perfil de usuário no model User

--- src/main/java/com/caiomorceli/todosimple/model/User.java
+++ src/main/java/com/caiomorceli/todosimple/model/User.java
@@ -1,7 +1,10 @@
 package com.caiomorceli.todosimple.model;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,7 +14,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.caiomorceli.todosimple.model.enums.ProfileEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -22,8 +25,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -47,12 +52,12 @@ public class User {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "username", length = 50, nullable = false, unique = true)
+    @Column(name = "username", length = 80, nullable = false, unique = true)
     @NotNull(groups = CreateUser.class)  // Não aceita valor null no atributo.
     @NotEmpty(groups = CreateUser.class) // Não aceita string vazia no atributo.
     private String username;
 
-    @Column(name = "password", length = 50, nullable = false)
+    @Column(name = "password", length = 80, nullable = false)
     @JsonProperty(access = Access.WRITE_ONLY) // Atributo apenas de escrita. A api não retornará esse atributo no json.
     @NotNull(groups = { CreateUser.class, UpdateUser.class })
     @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
@@ -61,5 +66,23 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     @JsonProperty(access = Access.WRITE_ONLY)     // Atributo apenas de escrita. Não é retornado no JSON no momento da leitura.
-    private List<Task> tasks = new ArrayList<>();                
+    private List<Task> tasks = new ArrayList<>(); 
+    
+    @ElementCollection(fetch = FetchType.EAGER)   // Será uma tabela no BD que é uma coleção de perfis associados à tabela de usuário. FetchType.EAGER: Sempre que buscar os usuários, os perfis também são retornados 
+    @JsonProperty(access = Access.WRITE_ONLY)     // Atributo apenas de escrita. A api não retornará esse atributo no json.
+    @CollectionTable(name = "user_profile")       // Define o nome da tabela no BD.
+    @Column(name = "profile", nullable = false)   // Define o nome do atributo na tabela.
+    private Set<Integer> profiles = new HashSet<>();
+
+
+    // Retorna a lista com os ENUMS 
+    public Set<ProfileEnum> getProfiles(){
+        return this.profiles.stream().map(profile -> ProfileEnum.toEnum(profile)).collect(Collectors.toSet());
+    }
+
+    // Adiciona um novo código à lista de perfis.
+    public void addProfile(ProfileEnum profileEnum){
+        this.profiles.add(profileEnum.getCode());
+    }
+
 }
