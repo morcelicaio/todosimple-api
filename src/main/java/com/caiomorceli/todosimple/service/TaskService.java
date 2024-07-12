d601cc2038b5be3f02cbee5e5d641ccc3f62aba1
morcelicaio <morcelicaio@hotmail.com>
2024-07-12 15:06:28 -0300

    Adiciona verificação de usuário autenticado e perfil de usuário para realizar busca e insert de tasks

--- src/main/java/com/caiomorceli/todosimple/service/TaskService.java
+++ src/main/java/com/caiomorceli/todosimple/service/TaskService.java
@@ -1,7 +1,7 @@
 package com.caiomorceli.todosimple.service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,7 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.caiomorceli.todosimple.model.Task;
 import com.caiomorceli.todosimple.model.User;
+import com.caiomorceli.todosimple.model.enums.ProfileEnum;
 import com.caiomorceli.todosimple.repositorie.TaskRepository;
+import com.caiomorceli.todosimple.security.UserSpringSecurity;
+import com.caiomorceli.todosimple.service.exception.AuthorizationException;
 import com.caiomorceli.todosimple.service.exception.DataBindingViolationException;
 import com.caiomorceli.todosimple.service.exception.ObjectNotFoundException;
 
@@ -22,22 +25,77 @@ public class TaskService {
     @Autowired
     private UserService userService;
 
+    /*
     public Task findTaskById(Long id){
         Optional<Task> task = taskRepository.findById(id);
 
         return task.orElseThrow(() -> new ObjectNotFoundException(
             "Tarefa não encontrada! \n Id: "+ id + ", Tipo: "+Task.class.getName()));
+    }   */
+    public Task findTaskById(Long id){
+        Task task = taskRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+            "Tarefa não encontrada! - Id: "+ id + ", Tipo: "+Task.class.getName()));
+
+        // Recupera o usuário que está no contexto da aplicação (está logado).        
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+
+        // Se o usuário não está logado ou se não tem perfil de adm e a task não pertence a ele é disparada a exception.
+        if(Objects.isNull(userSpringSecurity)
+                || !userSpringSecurity.hasRole(ProfileEnum.ADMIN) && !this.userHasTask(userSpringSecurity, task)){
+
+            throw new AuthorizationException("Acesso negado!");
+        }        
+
+        return task;
     }
 
+    /*
     public List<Task> findAllTasksByUserId(Long userId){
         List<Task> tasks = this.taskRepository.findByUser_Id(userId);
         
         return tasks;
+    }       */
+
+    // Busca apenas do usuário que está logado e não mais pelo id.
+    public List<Task> findAllTasksByUser(){
+        // Recupera o usuário que está no contexto da aplicação (está logado).        
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+
+        // Se o usuário não está logado é disparada a exception.
+        if(Objects.isNull(userSpringSecurity)){
+            throw new AuthorizationException("Acesso negado!");
+        }
+
+        // Utiliza o id do usuário que está logado para fazer a busca.
+        List<Task> tasks = this.taskRepository.findByUser_Id(userSpringSecurity.getId());
+        
+        return tasks;
     }
 
+    /*
     @Transactional
     public Task createTask(Task task){
         User user = this.userService.findUserById(task.getUser().getId());
+
+        task.setId(null);
+        task.setUser(user);
+        task = this.taskRepository.save(task);
+
+        return task;
+    }       */
+
+    @Transactional
+    public Task createTask(Task task){
+        // Recupera o usuário que está no contexto da aplicação (está logado).        
+        UserSpringSecurity userSpringSecurity = UserService.authenticated();
+
+        // Se o usuário não está logado é disparada a exception.
+        if(Objects.isNull(userSpringSecurity)){
+            throw new AuthorizationException("Acesso negado!");
+        }
+
+        // Utiliza o id do usuário que está logado para fazer a busca.
+        User user = this.userService.findUserById(userSpringSecurity.getId());
 
         task.setId(null);
         task.setUser(user);
@@ -64,5 +122,10 @@ public class TaskService {
                 // Não exclui caso a task tenha outras entidades relacionadas a ela.
                 throw new DataBindingViolationException("Não é possível excluir pois há entidades relacionadas.");
             }
+    }
+
+    // Verifica se o id do dono da task é o mesmo do usuário que está logado. Se o usuário é dono dessa task.
+    private Boolean userHasTask(UserSpringSecurity userSpringSecurity, Task task){        
+        return task.getUser().getId().equals(userSpringSecurity.getId());                
     }
 }
