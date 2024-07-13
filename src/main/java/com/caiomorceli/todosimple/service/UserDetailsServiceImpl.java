a282b3cab3b53de82444fb7853a8a8eb06089130
morcelicaio <morcelicaio@hotmail.com>
2024-07-12 21:46:33 -0300

    Refatora a clase UserDetailsServiceImpl

--- src/main/java/com/caiomorceli/todosimple/service/UserDetailsServiceImpl.java
+++ src/main/java/com/caiomorceli/todosimple/service/UserDetailsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.caiomorceli.todosimple.model.User;
-import com.caiomorceli.todosimple.repositorie.UserRepository;
+import com.caiomorceli.todosimple.repository.UserRepository;
 import com.caiomorceli.todosimple.security.UserSpringSecurity;
 
 @Service
